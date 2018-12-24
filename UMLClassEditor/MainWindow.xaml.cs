89317687e930b1779445ce12486198930c163ed9
MASLINA <smavmc@gmail.com>
2018-12-24 19:50:58 +0500

    lines

--- UMLClassEditor/MainWindow.xaml.cs
+++ UMLClassEditor/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace UMLClassEditor {
     public partial class MainWindow : Window {
         public enum State {
             Editing, ClassBox, InterfaceBox, AssotiationArrow, DerivArrow, ImplementationArrow, DependenceArrow, AggregationArrow, CompositionArrow
-
+                //
         }
 
         public MainWindow() {

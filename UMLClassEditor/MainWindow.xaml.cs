19fb9f19b7b324b0ffaae34e5dd3e7e121bd97e4
vexstar <ivexgod@gmail.com>
2018-12-24 19:51:56 +0500

    kjr

--- UMLClassEditor/MainWindow.xaml.cs
+++ UMLClassEditor/MainWindow.xaml.cs
@@ -1,21 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using UMLClassEditor.DrawElements.Arrows;
-using UMLClassEditor.DrawElements.Blocks;
-using UMLClassEditor.DrawElements.Lines;
-using UMLClassEditor.DrawElements.Tips;
+using UMLClassEditor.DrawElements;
 
 namespace UMLClassEditor {
     /// <summary>
@@ -27,7 +13,7 @@ namespace UMLClassEditor {
             
         }
 
-
+        List<UMLElement> elements = new List<UMLElement>();
         public MainWindow() {
             InitializeComponent();
             drawCanvas.PreviewMouseMove += DrawCanvasOnPreviewMouseMove;
@@ -44,6 +30,10 @@ namespace UMLClassEditor {
 
         private void Canvas_MouseUp(object sender, MouseButtonEventArgs e) {
 
+        }
+
+        private void ClassSelected_Click(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
